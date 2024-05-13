ab364703bd3896f0585a31ca9dfc265a0c68e786
Matheus Amon <amonmatheus757@gmail.com>
2024-05-13 18:04:18 -0300

    feat: add footer component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Footer from './components/footer'
 import Gallery from './components/gallery'
 import Header from './components/header'
 
@@ -8,6 +9,7 @@ function App() {
     <>
       <Header />
       <Gallery />
+      <Footer />
     </>
   )
 }
---
+++ src/components/footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return ( 
+        <footer className="w-full py-10 mt-10 bg-green-950 px-2">
+            <span className="text-white block text-center ">&copy; 2024 Matheus Dev. Todos os direitos reservados.</span>
+        </footer>
+     );
+}
+ 
+export default Footer;
